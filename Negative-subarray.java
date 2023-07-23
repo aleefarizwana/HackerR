4d2da0ed455dc67b0ffd414fcf214ac043b9bd9e
aleefarizwana <126440963+aleefarizwana@users.noreply.github.com>
2023-07-23 21:08:04 +0530

    Hacker Rank
    simple solution of hacker rank problem.

---
+++ Negative-subarray.java
@@ -0,0 +1,36 @@
+The sum of an array is the total sum of its elements.
+An array's sum is negative if the total sum of its elements is negative.
+An array's sum is positive if the total sum of its elements is positive.
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        scan.close();
+        int count = 0;
+        for(int i = 0;i<n;i++){
+            int tot = arr[i];
+            if (tot < 0){             //ele is negative
+                count += 1;
+            }
+            for(int j = i+1;j<n;j++){    
+                tot += arr[j];             //adding all ele after that ele 
+                if (tot < 0){   //if that tot less                             
+                    count += 1; //than zero 
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    }
+
